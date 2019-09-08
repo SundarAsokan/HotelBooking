22b80f61ae5d2e04ccf44a6880e27861821da71e
jameelmohammad <55061066+jameelmohammad@users.noreply.github.com>
2019-09-08 15:08:33 -0400

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            string baseUrl= "http://localhost:51391/api/";
+            string username = "aaa";
+            string password = "baa";
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(baseUrl);
+
+                string queryString = string.Format("values/login?username={0}&Password={1}", username, password);
+                var httpResponse = httpClient.GetAsync(queryString);
+                HttpResponseMessage httpResponseMessage = httpResponse.GetAwaiter().GetResult();
+                if (httpResponseMessage.IsSuccessStatusCode)
+                {
+                    var resultset = httpResponseMessage.Content.ReadAsStringAsync();//   .Parse(httpResponseMessage.Content.ToString());// ReadAsStringAsync().ToString());
+                    //var resultset = JObject.Parse(httpResponseMessage.Content.ToString());// ReadAsStringAsync().ToString());
+                    string Result = JsonConvert.DeserializeObject (resultset.Result.ToString()).ToString();
+                }
+            }
+
+
+
+                ViewBag.Title = "Home Page";
+
+            return View();
+        }
+    }
+}
---
+++ ValuesController.cs
@@ -0,0 +1,70 @@
+﻿using HotelAPI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HotelAPI.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        [HttpGet]
+        public IHttpActionResult Login([FromUri] string userName, string Password)        
+        {
+            var LoginResult="";
+
+            HotelBookingTeam4Entities obj = new HotelBookingTeam4Entities();
+            userName = userName ?? string.Empty;
+
+            var userResult = obj.tblUsers.Where(user => (user.Name ?? string.Empty).ToUpper() == userName.ToUpper() && user.Password == Password).FirstOrDefault();
+
+
+            if (userResult != null)
+            {
+                LoginResult = "success";
+            }
+            else
+            {
+                LoginResult = "Fail";
+            }
+            //return Ok(JsonConvert.SerializeObject(LoginResult));
+            return Ok(JsonConvert.SerializeObject( LoginResult));
+        }
+
+
+        [HttpPost]
+        public IHttpActionResult Register([FromUri] UserDetails userDetails)
+        {
+            
+
+            return Ok();
+        }
+
+
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
